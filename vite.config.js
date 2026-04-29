b1525d534e7d991f4897ecfaa7b33bb180592743
Stefan4012IT <stefanra555@gmail.com>
2026-04-29 17:52:40 +0200

    Initial Unijaprojekt 2026 landing setup

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: '/unijaprojekt-2026/',
-})
+  base: command === 'build' ? '/unijaprojekt-2026/' : '/',
+}))
